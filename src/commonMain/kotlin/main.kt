a069152b621e94bd0278de4eeb718390d647dfcb
Patrick Mendonca <mendonca.patrick@yahoo.com>
2023-01-20 17:31:43 +0800

    first throw works, trying to set a "new game" button

--- src/commonMain/kotlin/main.kt
+++ src/commonMain/kotlin/main.kt
@@ -1,13 +1,12 @@
 import com.soywiz.klock.*
 import com.soywiz.korge.*
-import com.soywiz.korge.input.*
 import com.soywiz.korge.time.*
 import com.soywiz.korge.view.*
-import com.soywiz.korge.view.Circle
 import com.soywiz.korim.color.*
 import com.soywiz.korim.format.*
 import com.soywiz.korio.async.*
 import com.soywiz.korio.file.std.*
+import com.soywiz.korma.math.*
 import java.time.LocalDateTime
 
 
@@ -21,116 +20,154 @@ suspend fun main() = Korge(
     virtualHeight = 720,
 ) {
 
-    val backDrop = solidRect(width = 1280, height = 720, color = Colors.WHITE)
-    stage.addChild(backDrop)
+    // create object for even handling
+    val input = views.input
 
-    val deleteZone = solidRect(width = 10.0, height = 50.0, color = Colors.RED)
+    // create viewable area window
+    val window = solidRect(stage.actualVirtualWidth, stage.actualVirtualHeight)
+    window.centerOnStage()
+    window.alpha = 0.0
+
+
+
+
+    val inertia = Inertia()
+    val physics = Physics()
+
+    val halfX = (stage.actualVirtualWidth / 2)
+    val halfY = (stage.actualVirtualHeight / 2)
+    debug("i think stage half is $halfX, $halfY")
+
+
+    // create a collision object for scrolling item deletion. we place this item offstage to the left
+    // and watch for collisions to delete objects.
+    val deleteZone = solidRect(width = 10.0, height = (stage.height * 2.0), color = Colors.RED)
     deleteZone.centerOnStage()
     deleteZone.x = -(stage.width * 2.0)
     stage.addChild(deleteZone)
 
-
+    // create the background layer
+    // TODO CAN YOU MAKE A STAGE? ANOTHER VIEW? FIND OUT
+    val backDropContainer = mutableListOf<Any>()
+    // backDrop is the skybox behind the scrolling images in the background layer
+    val skyBox = solidRect(width = stage.width * 2, height = stage.height * 2, color = Colors.WHITE)
+    backDropContainer.add(skyBox)
+    // all other layers are added in front of the skybox
     val cityImage = resourcesVfs["city.png"].readBitmap().toBMP32()
     val skyline = image(cityImage)
     skyline.y = stage.height - skyline.height
-    stage.addChild(skyline)
+    backDropContainer.add(skyline)
+    val groundLayer = solidRect(width = stage.width * 1.5, height = 50.0, color = Colors.LIGHTGREEN)
+    groundLayer.centerOnStage()
+    groundLayer.y = (stage.height - (groundLayer.height / 2))
+    backDropContainer.add(groundLayer)
 
-    val input = views.input
+    for (item in backDropContainer) stage.addChild(item as View)
 
-    // MOUSE TRACKING BLOCK, CAN MARK OUT LATER
-    text("$mouseXY", color = Colors.BLACK) {
-        debug("mouse tracker launched", "mouseTracker")
+
+    // create the avatar
+    var launched = false
+    val avatar = circle(
+        radius = 20.0,
+        stroke = Colors.BLACK,
+        strokeThickness = 2.0,
+        fill = Colors.RED,
+        )
+    avatar.x = 0.0
+    avatar.y = (stage.height - (avatar.radius * 2.0))
+    stage.addChild(avatar)
+
+
+    // create the score
+    var distance = 0.0
+    val scoreBox = text("0.00  meters", color = Colors.BLACK)
+    scoreBox.addUpdater {
+        text = "${distance.roundDecimalPlaces(2)} meters"
+        scoreBox.alignTopToTopOf(window, padding = 10)
+        scoreBox.alignRightToRightOf(window, padding = 10)
+    }
+
+    addFixedUpdater(stage.gameWindow.timePerFrame) {
+        if (launched) {
+            // we check to see if we are done, if run is negative that means we have
+            // hit the ground and skidded to a stop, set all movement to 0 and tally score
+            if (inertia.run < 0) {
+                inertia.rise = 0.0
+                inertia.run = 0.0
+
+            }
+            // avatar physics slows and falls
+            avatar.x += inertia.run
+            avatar.y -= inertia.rise    // rise moves toward 0, the top of the window, so we subtract to float
+            inertia.run *= physics.windResist
+            inertia.rise += physics.gravity
+
+            // count score
+            distance += inertia.run / 10
+
+            // landing
+            if (avatar.collidesWith(groundLayer)) {
+                // TODO CAN WE BOUNCE HERE? DEPENDS ON INCOMING SPEED
+                inertia.rise = 0.0
+                if (inertia.run > 0) inertia.run -= physics.friction
+            }
+        } else {
+            if (input.mouseButtons > 0) {
+                debug("input, launching", "fixedUpdater")
+                inertia.rise += 30.0
+                inertia.run += 10
+                launched = true
+            }
+        }
+    }
+
+    // avatar info debug
+    text("", color = Colors.BLACK) {
         val text = this
         launchImmediately {
+            text.x = 5.0
+            text.y = 5.0
+            debug("avatar info box launched")
+            while(true) {
+                text.text = "XY: ${avatar.x + avatar.radius}, ${avatar.y + avatar.radius} \nInertia: ${inertia.rise}, ${inertia.run}"
+                delay(stage?.gameWindow?.timePerFrame ?: 1.milliseconds)
+            }
+        }
+    }
+
+    // MOUSE TRACKING BLOCK, CAN MARK OUT LATER
+    // NEEDS TO COME LAST TO BE RENDERED ON TOP LAYER
+    text("$mouseXY", color = Colors.BLACK) {
+        val text = this
+        launchImmediately {
+            debug("mouse tracker launched", "mouseTracker")
             while (true) {
                 text.text = "$mouseXY"
                 text.x = mouseX + 10
                 text.y = mouseY + 10
-                delay(1.milliseconds)
+                delay(stage?.gameWindow?.timePerFrame ?: 1.milliseconds)
             }
-        }
-    }
-
-    // create the avatar on the stage, pass input for listeners
-    val myAvatar = Avatar(stage, input)
-
-
-}
-
-
-class Avatar(stage: Stage, input: Input) : Circle() {
-
-    private var rise: Float = 30.0F // vertical movement
-    private var run: Float = 30.0F  // horizontal movement
-    private var launched: Boolean = false // have we launched
-    private var halfX = (stage.width / 2).toInt()
-    private var halfY = (stage.height / 2).toInt()
-
-    // TODO MAKE GRAVITY AND AIR RESISTANCE CONSTANTS HERE FUNCTION MAYBE?
-
-    init {
-        debug("creating avatar", "avatar.init()")
-        radius = 20.0
-        stroke = Colors.BLACK
-        strokeThickness = 2.0
-        fill = Colors.RED
-        x = 0.0
-        y = stage.height - (radius * 2)
-
-        stage.addChild(this)
-
-
-        debug("adding avatar updater", "avatar.init()")
-        addFixedUpdater(60.timesPerSecond) {
-
-            if (launched) {
-                moveAvatar()
-                gravity()
-                airDrag()
-
-                checkFloor()
-            } else {
-                showLaunchLine()
-                if (input.mouseButtons > 0) {
-                    debug("input, launching", "avatarUpdater")
-                    launched = true
-                }
-
-            }
-        }
-    }
-
-    private fun showLaunchLine() {
-        // TODO IMPLEMENT THE LINE TO SHOW LAUNCH ANGLE
-    }
-
-    private fun checkFloor() {
-        // TODO Do I run into the floor here?
-    }
-
-    private fun moveAvatar() {
-        if (x > halfX) {
-
-        } else {
-            x += run
-            y -= rise
-        }
-    }
-
-    private fun gravity() {
-        rise -= 0.5F
-    }
-
-    private fun airDrag() {
-        run -= when {
-            run > 40 -> 1.5F
-            run > 20 -> 0.5F
-            run > 3 -> 0.25F
-            else -> 0F
         }
     }
 }
 
+
+data class Inertia(
+    var rise: Double = 0.0,
+    var run: Double = 0.0,
+    )
+
+/**
+ * physics holds items we use to calculate movement changes.
+ * gravity is a constant that we expect to be subtracted from vertical movement (inertia.rise)
+ * windResist is a constant that we expect to be multiplied against horizontal movement (inertia.run)
+ * friction is a constant that we expect to be subtracted from horizontal movement on the ground (inertia.run)
+ */
+data class Physics(
+    val gravity: Double = -0.8,
+    val windResist: Double = 0.9999,
+    val friction: Double = 1.0,
+    )
 
 fun debug(message: String, name: String = "UNKNOWN") {
     val now = LocalDateTime.now()
