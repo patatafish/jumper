28c309567515441e0feda2bce510545a5772efc1
Patrick Mendonca <mendonca.patrick@yahoo.com>
2023-01-31 22:27:22 +0800

    working on skybox

--- src/commonMain/kotlin/main.kt
+++ src/commonMain/kotlin/main.kt
@@ -10,14 +10,25 @@ import com.soywiz.korma.math.*
 import java.time.LocalDateTime
 
 // needed for debug options at end of main
-//import com.soywiz.korio.async.*
-//import com.soywiz.korge.time.*
-
-
-// TODO: THE END OF GAME MENU LOOPS CREATION, FIND A WAY TO FIX.
+import com.soywiz.korio.async.*
+import com.soywiz.korge.time.*
 
 const val DEBUG_COLOR = "\u001b[33;1m"
 const val RESET_COLOR = "\u001b[0m"
+
+
+/**
+ *
+ * TODO LIST:
+ * sky color isnt darkening, figure out why
+ * tile the background items
+ * add further layers back
+ * show direction of avatar (line for now?) maybe trailing dot
+ * add bounce items
+ * make sure delete zone is working (create a create/delete log?)
+ */
+
+
 
 suspend fun main() = Korge(
     width = 1280,
@@ -39,6 +50,12 @@ suspend fun main() = Korge(
     window.centerOnStage()
     window.alpha = 0.0
 
+    // flag for creating game over menu items
+    var menuShow = false
+
+    // color control for the skybox
+    val skyBoxColor = SkyColor()
+    debug(skyBoxColor.getHex())
 
     val inertia = Inertia()
     val physics = Physics()
@@ -51,30 +68,37 @@ suspend fun main() = Korge(
     // create a collision object for scrolling item deletion. we place this item offstage to the left
     // and watch for collisions to delete objects.
     debug("creating collision object")
-    val deleteZone = solidRect(width = 10.0, height = (stage.height * 2.0), color = Colors.RED)
+    val deleteZone = solidRect(width = 10.0, height = (stage.height * 1000.0))
     deleteZone.centerOnStage()
-    deleteZone.x = -100.0
+    deleteZone.x = -100.0   // for testing delete, to be commented out later
 //    deleteZone.x = -(stage.width * 2.0)
+    // keep delete zone centered no matter the altitude
+    deleteZone.addUpdater { deleteZone.centerYOnStage() }
     stage.addChild(deleteZone)
 
     // create the background layer
     // TODO CAN YOU MAKE A STAGE? ANOTHER VIEW? FIND OUT
-    val backDropContainer = mutableListOf<Any>()
+    val backDropContainer = container()
     // backDrop is the skybox behind the scrolling images in the background layer
-    val skyBox = solidRect(width = stage.width * 2, height = stage.height * 2, color = Colors.WHITE)
-    backDropContainer.add(skyBox)
+    val skyBox = solidRect(
+        width = stage.width * 2,
+        height = stage.height * 2,
+        color = Colors["#" + skyBoxColor.getHex()],
+    )
+    backDropContainer.addChild(skyBox)
     // all other layers are added in front of the skybox
     val cityImage = resourcesVfs["city.png"].readBitmap().toBMP32()
     val skyline = image(cityImage)
     skyline.y = stage.height - skyline.height
-    backDropContainer.add(skyline)
-    val groundLayer = solidRect(width = stage.width * 1.5, height = 50.0, color = Colors.LIGHTGREEN)
+    backDropContainer.addChild(skyline)
+    val groundLayer = solidRect(
+        width = stage.width * 1.5,
+        height = 50.0,
+        color = Colors.LIGHTGREEN,
+    )
     groundLayer.centerOnStage()
     groundLayer.y = (stage.height - (groundLayer.height / 2))
-    backDropContainer.add(groundLayer)
-
-    for (item in backDropContainer) stage.addChild(item as View)
-
+    backDropContainer.addChild(groundLayer)
 
     // create the avatar
     debug("creating the avatar")
@@ -102,12 +126,90 @@ suspend fun main() = Korge(
         scoreBox.alignRightToRightOf(window, padding = 10)
     }
 
-    // updater to create end of game screen
-    window.addFixedUpdater(1.timesPerSecond) {
+
+    debug("adding the updater")
+    addFixedUpdater(30.timesPerSecond) {
+//    addFixedUpdater(stage.gameWindow.timePerFrame) {
+
+        // we have launched, but it is not game over
+        if (launched && !gameOver) {
+            // we check to see if we are done, if run is negative that means we have
+            // hit the ground and skidded to a stop, set all movement to 0 and tally score
+            if (inertia.run <= 0) {
+                debug("Round has ended, score ${avatarActualX.roundDecimalPlaces(2)}")
+                inertia.rise = 0.0
+                inertia.run = 0.0
+                // set launched to false to stop updating avatar
+                launched = false
+                // set flags to get game over menu
+                gameOver = true
+            }
+
+
+            // we do the actual movement of the avatar here, later on we position it on screen
+            avatarActualX += inertia.run
+            avatarActualY -= inertia.rise
+
+
+            // if the avatar has moved past mid-screen, we scroll the background, not move the avatar
+            if (avatarActualX <= halfX) {
+                // if the avatar hasn't moved yet to halfway across the screen, move the avatar
+                avatar.x = avatarActualX
+            } else {
+                // else we want to scroll the background behind the avatar
+                skyline.x -= (inertia.run * 0.9)
+            }
+
+            // if the avatar has moved past mid-screen, we move the background up or down
+            when (avatarActualY) {
+                // when we are within 1/2 window height, move the avatar
+                in halfY.toDouble()..(stage.actualVirtualHeight + avatar.radius) -> {
+                    avatar.y = avatarActualY
+                    groundLayer.visible = true
+                }
+                // otherwise we keep the avatar static and move background
+                else -> {
+                    avatar.y = halfY.toDouble()
+                    // move the background in a parallax manner
+                    groundLayer.visible = false
+                    skyline.y += (inertia.rise * 0.5)
+                }
+            }
+
+            // darken the skybox the higher we go, lighten it as we approach 0 altitude
+            skyBoxColor.darken()
+            debug(skyBoxColor.getHex())
+            skyBox.color = Colors["#${skyBoxColor.getHex()}"]
+
+
+            // We adjust the inertia using the physics numbers here, after all movement is completed this frame
+            inertia.rise += physics.gravity
+            inertia.run *= physics.windResist
+
+            // landing
+            if (avatar.collidesWith(groundLayer)) {
+                // TODO CAN WE BOUNCE HERE? DEPENDS ON INCOMING SPEED
+                inertia.rise = 0.0
+                if (avatarActualY < groundLayer.y) avatar.alignBottomToTopOf(groundLayer)
+                if (inertia.run > 0) inertia.run -= physics.friction
+            }
+        } else if (!gameOver) { // we have not launched, but still not game over (before round starts)
+            if (input.mouseButtons > 0) {
+                debug("input, launching", "fixedUpdater")
+                debug("mouse ${input.mouseButtons}", "fixedUpdater")
+                /** HERE IS THE KICK VALUES
+                 *8888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
+                 */
+                avatar.alignBottomToTopOf(groundLayer, 10)
+                inertia.rise += 50.0
+                inertia.run += 50.0
+                launched = true
+            }
+        }
         // game over actions
-        /// this is outside updater loop, all option must update self
-        if (gameOver) {
+        if (gameOver && !menuShow) {
             debug("game over menu start")
+            menuShow = true
 
             // define menu button sizes
             val buttonPadding = 35
@@ -153,7 +255,7 @@ suspend fun main() = Korge(
             // TODO: COUNT UP SCORE, THEN SHOW POINTS EARNED
             // TODO: SHOW HIGH SCORE, BONUS PTS
             val gameOverScore = text(
-                text = "${avatarActualX.roundDecimalPlaces(2)} meters",
+                text = "${(avatarActualX / 10).roundDecimalPlaces(2)} meters",
                 textSize = 48.0,
                 color = Colors.YELLOW,
                 font = scoreFont,
@@ -242,6 +344,8 @@ suspend fun main() = Korge(
 
                         debug("removing game over screen...")
                         gameOverContainer.removeChildren()
+                        gameOverContainer.parent?.removeChild(gameOverContainer)
+                        menuShow = false
 
                         debug("reset inertia values")
                         inertia.rise = 0.0
@@ -261,116 +365,45 @@ suspend fun main() = Korge(
                         avatar.alignBottomToTopOf(groundLayer, 10)
                         avatarActualX = avatar.x
                         avatarActualY = avatar.y
+
+                        debug("empty mouse input")
+                        input.mouseButtons = 0
                     }
                 }
             }
         }
     }
 
-    debug("adding the updater")
-    addFixedUpdater(30.timesPerSecond) {
-//    addFixedUpdater(stage.gameWindow.timePerFrame) {
-        if (launched && !gameOver) {
-            // we check to see if we are done, if run is negative that means we have
-            // hit the ground and skidded to a stop, set all movement to 0 and tally score
-            if (inertia.run <= 0) {
-                debug("Round has ended, score ${avatarActualX.roundDecimalPlaces(2)}")
-                inertia.rise = 0.0
-                inertia.run = 0.0
-                launched = false
-                gameOver = true
-            }
 
-
-            // we do the actual movement of the avatar here, later on we position it on screen
-            avatarActualX += inertia.run
-            avatarActualY -= inertia.rise
-
-
-            // TODO: CHECK MOVEMENT HERE FOR RISE AND RUN, KEEP THE AVATAR CENTERED
-            // if the avatar has moved past mid-screen, we scroll the background, not move the avatar
-            if (avatarActualX <= halfX) {
-                // if the avatar hasn't moved yet to halfway across the screen, move the avatar
-                avatar.x = avatarActualX
-            } else {
-                // else we want to scroll the background behind the avatar
-                skyline.x -= (inertia.run * 0.9)
-            }
-
-            // TODO: WE NEED TO SCROLL THE BACKGROUND UP AND DOWN WITH THE RISE OF THE AVATAR
-            // if the avatar has moved past mid-screen, we move the background up or down
-            when (avatarActualY) {
-                in halfY.toDouble()..(stage.actualVirtualHeight + avatar.radius) -> {
-                    avatar.y = avatarActualY
-                    groundLayer.visible = true
-                }
-
-                else -> {
-                    avatar.y = halfY.toDouble()
-                    // move the background in a parallax manner
-                    groundLayer.visible = false
-                    skyline.y += (inertia.rise * 0.5)
-                }
-            }
-
-//            avatar.x = avatarActualX
-//            avatar.y = avatarActualY
-
-            // We adjust the inertia using the physics numbers here, after all movement is completed this frame
-            inertia.rise += physics.gravity
-            inertia.run *= physics.windResist
-
-            // landing
-            if (avatar.collidesWith(groundLayer)) {
-                // TODO CAN WE BOUNCE HERE? DEPENDS ON INCOMING SPEED
-                inertia.rise = 0.0
-                if (inertia.run > 0) inertia.run -= physics.friction
-            }
-        } else {
-            if (input.mouseButtons > 0) {
-                debug("input, launching", "fixedUpdater")
-                debug("mouse ${input.mouseButtons}", "fixedUpdater")
-                /** HERE IS THE KICK VALUES
-                 *8888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
-                 */
-                avatar.alignBottomToTopOf(groundLayer, 10)
-                inertia.rise += 20.0
-                inertia.run += 30.0
-                launched = true
+    // avatar info debug
+    text("", color = Colors.BLACK) {
+        val text = this
+        launchImmediately {
+            text.x = 5.0
+            text.y = 5.0
+            debug("avatar info box launched")
+            while (true) {
+                text.text =
+                    "XY: ${avatarActualX + avatar.radius}, ${(avatarActualY + avatar.radius)} \nInertia: ${inertia.rise}, ${inertia.run}"
+                delay(stage?.gameWindow?.timePerFrame ?: 1.milliseconds)
             }
         }
     }
 
-
-//    // avatar info debug
-//    text("", color = Colors.BLACK) {
-//        val text = this
-//        launchImmediately {
-//            text.x = 5.0
-//            text.y = 5.0
-//            debug("avatar info box launched")
-//            while (true) {
-//                text.text =
-//                    "XY: ${avatarActualX + avatar.radius}, ${(avatarActualY + avatar.radius)} \nInertia: ${inertia.rise}, ${inertia.run}"
-//                delay(stage?.gameWindow?.timePerFrame ?: 1.milliseconds)
-//            }
-//        }
-//    }
-
-//    // MOUSE TRACKING BLOCK, CAN MARK OUT LATER
-//    // NEEDS TO COME LAST TO BE RENDERED ON TOP LAYER
-//    text("$mouseXY", color = Colors.BLACK) {
-//        val text = this
-//        launchImmediately {
-//            debug("mouse tracker launched", "mouseTracker")
-//            while (true) {
-//                text.text = "$mouseXY"
-//                text.x = mouseX + 10
-//                text.y = mouseY + 10
-//                delay(stage?.gameWindow?.timePerFrame ?: 1.milliseconds)
-//            }
-//        }
-//    }
+    // MOUSE TRACKING BLOCK, CAN MARK OUT LATER
+    // NEEDS TO COME LAST TO BE RENDERED ON TOP LAYER
+    text("$mouseXY", color = Colors.BLACK) {
+        val text = this
+        launchImmediately {
+            debug("mouse tracker launched", "mouseTracker")
+            while (true) {
+                text.text = "$mouseXY"
+                text.x = mouseX + 10
+                text.y = mouseY + 10
+                delay(stage?.gameWindow?.timePerFrame ?: 1.milliseconds)
+            }
+        }
+    }
 }
 
 /**
@@ -394,6 +427,44 @@ data class Physics(
     val windResist: Double = 0.99,
     val friction: Double = 1.0,
 )
+
+class SkyColor {
+    private var red = 55
+    private var green = 200
+    private var blue = 255
+    private var hexString = "37C8FF"
+    private val colorList = listOf(red, green, blue)
+
+    fun getHex() = hexString
+
+    fun darken() {
+        if (red > 1) red--
+        if (green > 1) green--
+        if (blue > 1) blue--
+        setHex()
+    }
+
+    fun lighten() {
+        if (red < 55) red++
+        if (green < 200) green++
+        if (blue < 255) blue++
+        setHex()
+    }
+
+    private fun setHex () {
+        var tempString = ""
+        for (color in colorList) {
+            tempString += if (color < 16) {
+                "0" + color.toString(16)
+            } else {
+                color.toString(16)
+            }
+        }
+        hexString = tempString
+    }
+
+}
+
 
 fun debug(message: String, name: String = "UNKNOWN") {
     val now = LocalDateTime.now()
